85b3fa99d6eb115ac7c03ed4e13820d413464759
Datior <leonardowederveiga@yahoo.com>
2022-10-25 00:12:37 -0300

    Classe Position criada com sucesso

---
+++ src/boardgame/Position.java
@@ -0,0 +1,46 @@
+package boardgame;
+
+public class Position {
+    
+    // ATRIBUTOS
+    private int row;
+    private int column;
+    // ATRIBUTOS
+
+    // CONSTRUTORES 
+    public Position() {
+    }
+
+    public Position(int row, int column) {
+        this.row = row;
+        this.column = column;
+    }
+    // CONSTRUTORES 
+
+    // ENCAPSULAMENTO
+    public int getRow() {
+        return row;
+    }
+
+    public void setRow(int row) {
+        this.row = row;
+    }
+
+    public int getColumn() {
+        return column;
+    }
+
+    public void setColumn(int column) {
+        this.column = column;
+    }
+    // ENCAPSULAMENTO
+    
+    // METODOS
+    @Override
+    public String toString() {
+        return row + ", " + column;
+    }
+    // METODOS
+    
+
+}
